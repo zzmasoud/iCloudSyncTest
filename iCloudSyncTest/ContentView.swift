d2d8fd1838458dbc3ed6e6fb8ff3ade9e053b8d8
zzmasoud <zzmasoud@gmail.com>
2023-10-31 09:00:26 -0400

    Add WatchOS target and share files between iOS/WatchOS

--- iCloudSyncTest/ContentView.swift
+++ iCloudSyncTest/ContentView.swift
@@ -26,9 +26,6 @@ struct ContentView: View {
                 .onDelete(perform: deleteItems)
             }
             .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
                 ToolbarItem {
                     Button(action: addItem) {
                         Label("Add Item", systemImage: "plus")
---
+++ iCloudSyncTestWatch Watch App/iCloudSyncTestWatchApp.swift
@@ -0,0 +1,31 @@
+//
+//  Copyright © zzmasoud (github.com/zzmasoud).
+//  
+
+import SwiftUI
+import CoreData
+
+@main
+struct iCloudSyncTestWatch_Watch_AppApp: App {
+    
+    private let coreDataStore: CoreDataStore
+    
+    init() {
+        let storeURL = NSPersistentContainer
+            .defaultDirectoryURL()
+            .appendingPathComponent("store.sqlite")
+
+        do {
+            self.coreDataStore = try CoreDataStore(storeURL: storeURL, containerIdentifier: Static.cloudIdentifier)
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, coreDataStore.container.viewContext)
+        }
+    }
+}
